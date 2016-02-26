5b1a01131cfeda9a8630ae9baf7a80b6e96c8c7e
unknown <miurf@hotmail.fr>
2016-02-26 22:23:07 +0100

    adding fosuserbundle to manage users

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            // added bundles
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            // Pasteque bundles
             new Pasteque\Bundle\ServerBundle\PastequeServerBundle(),
             //new Pasteque\Bundle\APIBundle\PastequeAPIBundle(),
+
+            // vendor bundles
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
