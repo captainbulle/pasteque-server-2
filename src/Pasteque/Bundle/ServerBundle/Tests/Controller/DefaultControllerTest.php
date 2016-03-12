8f1bb46517b0457bb730d8d56455ac4f2e906468
captainbulle <jerome.radal@gmail.com>
2016-03-12 13:31:06 +0100

    first test

--- src/Pasteque/Bundle/ServerBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Pasteque/Bundle/ServerBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,15 +3,25 @@
 namespace Pasteque\Bundle\ServerBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use GuzzleHttp\Client as Client;
 
-class DefaultControllerTest extends WebTestCase
+class AttributeControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        /*$client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertContains('Hello World', $client->getResponse()->getContent());*/
+
+        // Create a client with a base URI
+        $client = new Client(['base_uri' => 'https://pasteque-server-2.com/api/']);
+
+        // Send a request to https://pasteque-server-2.com/api/test
+        $response = $client->request('GET', 'test');
+
+        // Send a request to https://pasteque-server-2.com/root
+        $response = $client->request('GET', '/root');
     }
 }
